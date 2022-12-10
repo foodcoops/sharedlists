3bfb4b4d0ceb6730a47168f246dda4364d64969c
wvengen <willem@thequestionmark.org>
2022-12-10 17:37:21 +0100

    Make login work in development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Sharedlists::Application.config.session_store :cookie_store, key: '_sharedLists_session', secure: true, path: ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
+Sharedlists::Application.config.session_store :cookie_store, key: '_sharedLists_session', secure: Rails.env.production?, path: ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
