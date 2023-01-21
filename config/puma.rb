754cff14ce713d797c5ed5a71aa909bc357f8b25
kidhab <kidhab@systemausfall.org>
2023-01-21 19:57:58 +0100

    Auto bind puma to systemd activated sockets

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,9 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 
+# To automatically bind to all systemd activated sockets
+bind_to_activated_sockets
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
