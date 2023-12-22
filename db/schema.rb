f8cce8df7afe79433cc2867091a5fbf3ab07be32
kidhab <kidhab@systemausfall.org>
2023-12-22 13:25:54 +0100

    feat: add foodcoop field to supplier Can be used in Foodsoft to filter for foodcoops in multisite setups Add default of "" to new foodcoop field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_150110) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_14_145104) do
   create_table "articles", force: :cascade do |t|
     t.string "name", null: false
     t.integer "supplier_id", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_150110) do
     t.string "mail_subject"
     t.string "mail_type"
     t.string "salt", null: false
+    t.string "foodcoop"
     t.index ["name"], name: "index_suppliers_on_name", unique: true
   end
 
