719c6c9fae2faffef4e6080e4c87944006679d8a
kidhab <kidhab@systemausfall.org>
2023-12-22 15:41:43 +0100

    Update format of cache entries

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Sharedlists
   class Application < Rails::Application
+    config.active_support.cache_format_version = 7.0
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
